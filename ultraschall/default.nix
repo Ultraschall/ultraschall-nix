a08a28f6d7775454925ffcabcec3b8e944c541c1
Udo Sauer <udo.sauer@bevuta.com>
2024-11-05 13:11:08 +0100

    WIP

--- ultraschall/default.nix
+++ ultraschall/default.nix
@@ -1,42 +1,41 @@
 {
   alsa-lib,
   autoPatchelfHook,
+  bash,
+  copyDesktopItems,
   curl,
   fetchurl,
   ffmpeg,
+  gnutar,
   gtk3,
   lame,
   lib,
   libjack2,
   libxml2,
+  makeDesktopItem,
   makeWrapper,
   openssl,
-  pkgs,
-  reaper,
   pulseaudio,
+  reaper,
   stdenv,
   vlc,
   which,
   xdg-utils,
   xdotool,
-  gnutar,
-  gnused,
-  copyDesktopItems,
-  makeDesktopItem,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "ultraschall";
-  #version = "V5.1_6_202410180942"; # official 5.1 release
-  version = "5.1.1_144_202411031527"; # pre-release
+  version = "V5.1.1_9_202412091312"; # official 5.1 release
+  #version = "5.1.1_148_202411131311"; # pre-release
 
   src = fetchurl {
     # official 5.1 release:
-    #url = "https://github.com/Ultraschall/ultraschall-installer/releases/download/${finalAttrs.version}/Ultraschall-5.1.tar.gz";
-    #hash = "sha256-LHSksmpsKd+X0kJdDCWX8TD7vt9QApwdTi+aIyVaNJs=";
+    url = "https://github.com/Ultraschall/ultraschall-installer/releases/download/${finalAttrs.version}/Ultraschall-5.1.1.tar.gz";
+    hash = "sha256-v5fpeUGmL0d+Fv0SoI93P0NbsnYqNO0hoMpRl0WA5wg=";
 
     # pre-release
-    url = "https://github.com/Ultraschall/ultraschall-installer/releases/download/R${finalAttrs.version}/ULTRASCHALL_R5.1.1-preview.tar.gz";
-    hash = "sha256-KWKDTd7ziXrE26VW6WjJ/BD5yZkok8wojwJJOXCljsU=";
+    #url = "https://github.com/Ultraschall/ultraschall-installer/releases/download/R${finalAttrs.version}/ULTRASCHALL_R5.1.1-preview.tar.gz";
+    #hash = "sha256-ofUtbrreMnO02qc8V2smZebnyqDPTd3dFdPf2lUHZ7A=";
   };
 
   nativeBuildInputs = [autoPatchelfHook copyDesktopItems makeWrapper xdg-utils which];
@@ -44,7 +43,7 @@ stdenv.mkDerivation (finalAttrs: {
   runtimeDependencies = [gtk3 libjack2 pulseaudio reaper];
 
   buildPhase = let
-    reaperPackage = pkgs.reaper.overrideAttrs (_: {
+    reaperPackage = reaper.overrideAttrs (_: {
       version = "6.83"; # Ultraschall demands the latest 6.x version
       src = fetchurl {
         url = "https://www.reaper.fm/files/6.x/reaper683_linux_x86_64.tar.xz";
@@ -61,14 +60,15 @@ stdenv.mkDerivation (finalAttrs: {
     cp "${icon}" ultraschall.png
 
     cat <<'EOF' > ultraschall
-      #! ${pkgs.bash}/bin/bash -e
+      #! ${lib.getExe bash}
+      set euo pipefail
       DIR=${builtins.placeholder "out"}
 
       # check if this script ran before for this ultraschall package:
       if [ -f "$HOME/.config/ULTRASCHALL/installedversion" ] && [[ "$(< "$HOME/.config/ULTRASCHALL/installedversion")" == "${finalAttrs.version}" ]] ; then
-        echo "starting ultraschall"
+        echo "Starting Ultraschall."
       else
-        echo "setting up ultraschall for the first time"
+        echo "Setting up Ultraschall for the first time. Installing everything to ~/.config/ULTRASCHALL."
         mkdir -p "$HOME"/.config/ULTRASCHALL/{UserPlugins,Scripts}
         mkdir -p "$HOME"/{.vst3,.lv2}
         cp -fr "$DIR/themes/"/* "$HOME/.config/ULTRASCHALL"
@@ -94,11 +94,10 @@ stdenv.mkDerivation (finalAttrs: {
 
     mkdir -p "$out/bin"
     mkdir -p "$out/share/icons/hicolor/512x512/apps"
-    
     mv ultraschall.png "$out/share/icons/hicolor/512x512/apps/ultraschall.png"
     mv ultraschall "$out/bin/ultraschall" && chmod +x "$out/bin/ultraschall"
     cp -r * "$out"
-    ${gnutar}/bin/tar xf themes/ultraschall-theme.tar -C "$out/themes/" && rm "$out/themes/ultraschall-theme.tar"
+    ${lib.getExe gnutar} xf themes/ultraschall-theme.tar -C "$out/themes/" && rm "$out/themes/ultraschall-theme.tar"
 
     runHook postInstall
   '';
